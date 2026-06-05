483aeb64410654c0083788c1cee294859b7e4f54
imannaswini <iammannaswini@gmail.com>
2026-06-05 09:34:37 +0530

    LC-253

--- Day1/src/LC/MeetingAllocator.java
+++ Day1/src/LC/MeetingAllocator.java
@@ -6,9 +6,35 @@ Pattern :
 */
 public class MeetingAllocator 
 {
- public static void main(String[] args) 
+    public static int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        java.util.Arrays.sort(start);
+        java.util.Arrays.sort(end);
+        int rooms = 0, maxRooms = 0;
+        int i = 0, j = 0;
+        while (i < n) {
+            if (start[i] < end[j]) {
+                rooms++;
+                maxRooms = Math.max(maxRooms, rooms);
+                i++;
+            } else {
+                rooms--;
+                j++;
+            }
+        }
+        return maxRooms;
+    }
+
+    public static void main(String[] args) 
  {
     int[][] meetings = {{0,30},{5,10},{15,20}};
-    
+    System.out.println(minMeetingRooms(meetings));
+
  }
 }
