90a912eca7c8e3314852b45bbf814575b5aaf3af
imannaswini <iammannaswini@gmail.com>
2026-06-05 09:25:49 +0530

    LC-253

---
+++ Day1/src/LC/MeetingAllocator.java
@@ -0,0 +1,14 @@
+package LC;
+/*
+Pattern : 
+   1. Two Pointer
+   2. Sorting (Sweep Line)
+*/
+public class MeetingAllocator 
+{
+ public static void main(String[] args) 
+ {
+    int[][] meetings = {{0,30},{5,10},{15,20}};
+    
+ }
+}
